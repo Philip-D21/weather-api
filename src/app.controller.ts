3c4f0a25393c1d29392d0722a77f4e2bf2a6201f
Philip-D21 <philiplekan88@gmail.com>
2024-07-04 23:29:33 +0100

    fix: ip ish

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,21 +7,22 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('hello')
-  async greet(@Req() request: Request, @Query('visitor_name') visitor_name: string) {
-    if (!visitor_name) {
+  async greet(@Req() request: Request, @Query('visitor-name') visitorName: string) {
+    if (!visitorName) {
       throw new HttpException('Visitor name parameter is missing', 400);
     }
 
+    // Extract IPv4 address from IPv6 mapped format if necessary
     let clientIp = request.ip;
-    if (clientIp === '::1') {
-      clientIp = '127.0.0.1';
+    if (clientIp.startsWith('::ffff:')) {
+      clientIp = clientIp.substr(7); // Remove '::ffff:' prefix to get IPv4 address
     }
 
     console.log(`Client IP: ${clientIp}`);
     const location = await this.appService.getLocation(clientIp);
     const weatherData = await this.appService.getWeather(location.city).toPromise();
 
-    return this.appService.createGreeting(clientIp, location.city, weatherData.main.temp, visitor_name);
+    return this.appService.createGreeting(clientIp, location.city, weatherData.main.temp, visitorName);
   }
 
   @Get('health')
